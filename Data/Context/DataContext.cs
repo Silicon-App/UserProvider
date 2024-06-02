7845acdb3c0a705e23aa89add1ef27b3768b6242
Albulena <AlbulenaShabani/html-css.git>
2024-06-02 15:42:58 +0200

    Add project files.

---
+++ Data/Context/DataContext.cs
@@ -0,0 +1,11 @@
+﻿
+using Data.Entitites;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Context;
+
+public class DataContext(DbContextOptions<DataContext> options) : IdentityDbContext<ApplicationUser>(options)
+{
+    public DbSet<AddressEntity> Addresses { get; set; }
+}
